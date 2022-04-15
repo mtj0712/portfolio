fee5a298479a9bb2d7de8933e468f410b8cb8b0f
mtj0712 <mtj0712@gmail.com>
2022-04-15 16:02:43 +0900

    Update sixDegrees.py
    Added comments to clarify what each code does. Declared a variable linkRe that holds the value of RegEx once for all to save time.

--- src/sixDegrees.py
+++ src/sixDegrees.py
@@ -8,34 +8,39 @@ from bs4 import BeautifulSoup
 import re
 import time
 
-start = "/wiki/Eric_Idle"
-end = "/wiki/Kevin_Bacon"
-
 beginTime = time.time()
+
+startUrl = "/wiki/Eric_Idle"
+endUrl = "/wiki/Kevin_Bacon"
+linkRe = re.compile(
+    "^(/wiki/)(?!(File|Help|Category|Special|Wikipedia|Template|Template_talk):)")
 
 try:
     # urls = list of (url, previous url index) tuples
-    urls = [(start, 0)]
+    urls = [(startUrl, 0)]
     # checkedUrls = set of checked urls
-    checkedUrls = {start}
+    checkedUrls = {startUrl}
 
     # BFS
     i = 0
     found = False
     while i < len(urls) and not found:
-        print(urls[i][0]) # debug
+        print("Currently exploring this page:", urls[i][0])
         html = urlopen("https://en.wikipedia.org" + urls[i][0])
         bs = BeautifulSoup(html, "html.parser")
 
-        for link in bs.find("div", id="bodyContent").findAll(
-            "a", href=re.compile("^(/wiki/)(?!(File|Help|Category|Special|Wikipedia|Template|Template_talk):)")
-        ):
-            # remove the id element of the url
+        for link in bs.find("div", id="bodyContent").findAll("a", href=linkRe):
+            # skip if this link doesn't have url
+            if "href" not in link.attrs:
+                continue
+            # remove the id part from the url
             url = link.attrs["href"].split("#", maxsplit=1)[0]
+            # add this url to the list if it's new
             if url not in checkedUrls:
                 checkedUrls.add(url)
                 urls.append((url, i))
-                if url == end:
+                # if the end page is reached, end
+                if url == endUrl:
                     found = True
                     break
         i += 1
@@ -49,7 +54,7 @@ try:
         
         # print out the result
         print("\n{} degrees from start to end!!!".format(len(steps)))
-        print(start)
+        print(startUrl)
         while len(steps) > 0:
             print(steps.pop())
     else:
