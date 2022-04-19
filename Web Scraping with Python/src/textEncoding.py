78a36612b108dab06b244299b69b36b1253680ff
mtj0712 <mtj0712@gmail.com>
2022-04-19 18:36:11 +0900

    Create textEncoding.py

---
+++ Web Scraping with Python/src/textEncoding.py
@@ -0,0 +1,21 @@
+# This code is based on a sample code from the book "Web Scraping with Python" by Ryan Mitchell.
+# This code reads text files from the Internet.
+
+from urllib.request import urlopen
+from urllib.error import HTTPError
+import time
+
+beginTime = time.time()
+
+# ASCII text
+asciiPage = urlopen("https://www.pythonscraping.com/pages/warandpeace/chapter1.txt")
+asciiText = str(asciiPage.read()).replace("\\n", "\n").replace("\\\'", "\'")
+print(asciiText)
+
+# UTF-8 text
+utf8Page = urlopen("https://www.pythonscraping.com/pages/warandpeace/chapter1-ru.txt")
+utf8Text = str(utf8Page.read(), "utf-8")
+print(utf8Text)
+
+endTime = time.time()
+print("Total time it took: {}s".format(endTime - beginTime))
